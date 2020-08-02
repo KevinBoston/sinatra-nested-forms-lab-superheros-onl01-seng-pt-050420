15a59e75bbda10aebe2e660d4a2254cba7bd5d72
Kevin Boston <kbos22@gmail.com>
2020-08-02 15:14:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,19 @@ class App < Sinatra::Base
     end
     
     post '/teams' do 
-      @team = Team.new(params)
-      erb :team
+    @team_name = params[:team][:name]
+    @team_motto = params[:team][:motto]
+    @hero_name = []
+    @hero_power = []
+    @hero_bio = []
+    @team_members = params[:team][:members]
+    @team_members.each do |hero|
+      @hero_name << hero[:name]
+      @hero_power << hero[:power]
+      @hero_bio << hero[:bio]
+    end
+
+    erb :team
     end
 
 
